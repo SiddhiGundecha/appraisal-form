49f93584b785d4a3dae0435ad1b7b06c770cbf8f
Vineet Y <vineety1105@gmail.com>
2026-02-19 14:47:57 +0530

    ~

--- src/pages/PrincipalDashboard.jsx
+++ src/pages/PrincipalDashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/HodDashboard.css";
+import "../styles/HODDashboard.css";
 import AppraisalSummary from "../components/AppraisalSummary";
 import {
   DEFAULT_TABLE2_VERIFIED_KEYS,
@@ -719,5 +719,6 @@ export default function PrincipalDashboard() {
     </div>
   );
 }
+
 
 
